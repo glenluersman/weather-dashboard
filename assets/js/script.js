4ba8469a63fe8c43a64efe0c69442c0f671e57e1
glenluersman <glen.luersman@gmail.com>
2021-12-05 11:30:14 -0500

    5 day forecast logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,62 +7,80 @@ var currentTempEl = document.querySelector("#temperature");
 var currentHumidityEl = document.querySelector("#humidity");
 var currentWindSpeedEl = document.querySelector("#wind-speed");
 var currentUVEl = document.querySelector("#UV-index");
+var forecastEls = document.querySelectorAll("#forecast");
 
-var getCurrentWeather = function() {
+
+var getWeather = function() {
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=troy,ohio&units=imperial&appid=" + apiKey;  
   fetch(apiUrl).then(function(response) {
-      if (response.ok) {
-        response.json().then(function(data) {
-          console.log(data);
-          displayCurrentWeather(data);
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data);
+        displayWeather(data);
         });
       } else {
         alert("Error: City not found");
       }
     });
-};
-
-var displayCurrentWeather = function(data) {
-  var currentDate = new Date(data.dt*1000);
-  console.log(currentDate); 
-  var month = currentDate.getMonth();
-  var day = currentDate.getDate();
-  var year = currentDate.getFullYear();
-  cityNameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
-  var weatherIcon = data.weather[0].icon;
-  weatherIconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-  weatherIconEl.setAttribute("alt", data.weather[0].description);
+  };
+  
+  var displayWeather = function(data) {
+    var currentDate = new Date(data.dt*1000);
+    console.log(currentDate); 
+    var month = currentDate.getMonth() +1;
+    var day = currentDate.getDate();
+    var year = currentDate.getFullYear();
+    cityNameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+    var weatherIcon = data.weather[0].icon;
+    weatherIconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+    weatherIconEl.setAttribute("alt", data.weather[0].description);
   currentTempEl.innerHTML = "Temperature: " + data.main.temp + "\u2109";
   currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + " %";
   currentWindSpeedEl.innerHTML = "Wind: " + data.wind.speed + " MPH";
   var lat = data.coord.lat;
   var lon = data.coord.lon;
-  var UVapiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  var UVapiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
   fetch(UVapiUrl).then(function(response) {
     if (response.ok) {
       response.json().then(function(oneCall) {
         console.log(oneCall);
-        currentUVEl.innerHTML = "UV Index: " + oneCall.current.uvi; 
+        currentUVEl.innerHTML = "UV Index: " + oneCall.current.uvi;
+        if (oneCall.current.uvi > 6) {
+          currentUVEl.setAttribute("class", "badge bg-danger");
+        } else {
+          currentUVEl.setAttribute("class", "badge bg-success");
+        }
+        
+        
+        for (i = 0; i < forecastEls.length; i++) {
+          forecastEls[i].innerHTML = "";
+          forecastDate = new Date(oneCall.daily[i].dt*1000);
+          var month = forecastDate.getMonth() +1;
+          var day = forecastDate.getDate();
+          var year = forecastDate.getFullYear();
+          var forecastDateEl = document.createElement("p");
+          forecastDateEl.setAttribute("class", "forecast-date mt-3 mb-0");
+          forecastDateEl.innerHTML = month + "/" + day + "/" + year;
+          forecastEls[i].appendChild(forecastDateEl);
+          var forecastIconEl = document.createElement("img");
+          forecastIconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + oneCall.daily[i].weather[0].icon + "@2x.png");
+          forecastIconEl.setAttribute("alt", oneCall.daily[i].weather[0].description);
+          forecastEls[i].appendChild(forecastIconEl);
+          var forecastTempEl = document.createElement("p");
+          forecastTempEl.innerHTML = "Temp: " + oneCall.daily[i].temp.day + "\u2109";
+          forecastEls[i].appendChild(forecastTempEl);
+          forecastHumidityEl = document.createElement("p");
+          forecastHumidityEl.innerHTML = "Humidity: " + oneCall.daily[i].humidity + " %";
+          forecastEls[i].appendChild(forecastHumidityEl);
+          forecastWindEl = document.createElement("p");
+          forecastWindEl.innerHTML = "Wind: " + oneCall.daily[i].wind_speed + " MPH";
+          forecastEls[i].appendChild(forecastWindEl);
+        }
       });
     }
   });
-}
+  
+};
 
-//var formSubmitHandler = function(event) {
-  //event.preventDefault();
-  //console.log(event);
-
-  // get value from input element
-  //var cityName = searchFormEl.value.trim();
-  //console.log(cityName);
-
-  //if (cityName) {
-    //getCurrentWeather(cityName);
-    //searchFormEl.value = "";
-    //} else {
-      //alert("Please enter a City");
-    //}
-//};
-
-//searchBtnEl.addEventListener("submit", formSubmitHandler);
-getCurrentWeather()
+//searchBtnEl.addEventListener("submit", getWeather);
+getWeather()
