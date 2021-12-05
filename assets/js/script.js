852c49cff78928bee66bab5100d4312249041f28
glenluersman <glen.luersman@gmail.com>
2021-12-04 22:25:07 -0500

    current weather displayed to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,51 @@
 var apiKey = "2d4a0de5c01a73e0bc35dafb10eadc48";
 var searchBtnEl = document.querySelector("#searchBtn");
 var searchFormEl = document.querySelector("#search-form");
-var currentWeatherEl = document.querySelector("#current-weather");
+var cityNameEl = document.querySelector("#city-name");
+var weatherIconEl = document.querySelector("#current-icon");
+var currentTempEl = document.querySelector("#temperature");
+var currentHumidityEl = document.querySelector("#humidity");
+var currentWindSpeedEl = document.querySelector("#wind-speed");
+var currentUVEl = document.querySelector("#UV-index");
 
 var getCurrentWeather = function() {
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;  
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=troy,ohio&units=imperial&appid=" + apiKey;  
   fetch(apiUrl).then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
           console.log(data);
           displayCurrentWeather(data);
-        })
+        });
       } else {
         alert("Error: City not found");
       }
-    })
+    });
 };
 
 var displayCurrentWeather = function(data) {
-  currentWeatherEl.textContent = data.name + data.main.humidity + data.main.temp + data.wind.speed + data.weather[0].icon;
-
+  var currentDate = new Date(data.dt*1000);
+  console.log(currentDate); 
+  var month = currentDate.getMonth();
+  var day = currentDate.getDate();
+  var year = currentDate.getFullYear();
+  cityNameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+  var weatherIcon = data.weather[0].icon;
+  weatherIconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+  weatherIconEl.setAttribute("alt", data.weather[0].description);
+  currentTempEl.innerHTML = "Temperature: " + data.main.temp + "\u2109";
+  currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + " %";
+  currentWindSpeedEl.innerHTML = "Wind: " + data.wind.speed + " MPH";
+  var lat = data.coord.lat;
+  var lon = data.coord.lon;
+  var UVapiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  fetch(UVapiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(oneCall) {
+        console.log(oneCall);
+        currentUVEl.innerHTML = "UV Index: " + oneCall.current.uvi; 
+      });
+    }
+  });
 }
 
 //var formSubmitHandler = function(event) {
